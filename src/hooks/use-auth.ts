6311d765405438827b535bfca77419a473802a85
Abdullah <abdullah.celik1409@gmail.com>
2026-04-12 21:56:00 +0300

    fix: useAuth another implicitly any type

--- src/hooks/use-auth.ts
+++ src/hooks/use-auth.ts
@@ -14,7 +14,7 @@ export function useAuth() {
       setLoading(false)
     })
 
-    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session: any) => {
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event: any, session: any) => {
       setUser(session?.user || null)
     })
 
